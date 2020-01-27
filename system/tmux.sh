0f3f7c6d21f7fb1996c0e128ca350a68f4d2422f
Husky Robot <husky@robot>
2020-01-27 18:16:54 +0100

    tmux script

---
+++ system/tmux.sh
@@ -0,0 +1,16 @@
+tmux rename-window -t 0 "Devices"
+tmux send-keys 'roslaunch mbzirc_husky robot.launch'
+tmux split-window -h
+sleep 0.2
+tmux send-keys 'roslaunch mbzirc_husky velodyne.launch'
+tmux split-window 
+sleep 0.2
+tmux send-keys 'roslaunch mbzirc_husky arm.launch'
+tmux split-window 
+sleep 0.2
+tmux send-keys 'roslaunch mbzirc_husky pump.launch'
+tmux split-window
+sleep 0.2
+tmux send-keys 'roslaunch mbzirc_husky realsense.launch'
+tmux new-window -n:"Slam"
+tmux send-keys 'roslaunch mbzirc_husky gmapping'
