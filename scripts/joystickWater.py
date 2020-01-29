363e865b709f8ad29ddf4f566f660ab036f3f350
Husky Robot <husky@robot>
2020-01-29 14:32:43 +0100

    water joy no longer constantly toggling

--- scripts/joystickWater.py
+++ scripts/joystickWater.py
@@ -7,17 +7,21 @@ import time
 
 pub = None
 
-signal = '"\x62\x01\x92\xF5"'
+signal = '\x62\x01\x92\xF5'
 output = '/dev/ttyACM0'
+state = "off"
 
 def callback(data):
-	global pub
-	if data.buttons[6] or data.buttons[7]:
+	global pub, state
+	if data.buttons[6] or data.buttons[7] and state == "off":
 
-            print("pressed")
-            subprocess.check_output(['echo', '-e', signal, '>', output])
-            time.sleep(0.1)
-            subprocess.check_output(['echo', '-e', signal, '>', output])
+            print("trig pressed firing water")
+            with open(output, "w") as f:
+                f.write(signal)
+            state = "on"
+            #time.sleep(0.04)
+            #with open(output, "w") as f:
+            #    f.write(signal)
             
             #print("pressed")
 		#msg = sprayActionGoal()
@@ -25,10 +29,18 @@ def callback(data):
 		#msg.goal.duration = 30
                 #print(msg)
 		#pub.publish(msg)
+        elif data.buttons[6] == 0 and data.buttons[7] == 0 and state == "on":
+            state = "off"
+            with open(output, "w") as f:
+                f.write(signal)
 
 if __name__ == '__main__':
 
 	rospy.init_node('joystickWaterPump', anonymous=True)
-	pub = rospy.Publisher('/sprayServer/goal', sprayActionGoal, queue_size=1)
+	#pub = rospy.Publisher('/sprayServer/goal', sprayActionGoal, queue_size=1)
 	rospy.Subscriber("/joy_teleop/joy", Joy, callback)
 	rospy.spin()
+
+        if state == "on":
+            with open(output, "w") as f:
+                f.write(signal)
