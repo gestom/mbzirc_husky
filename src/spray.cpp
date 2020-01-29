6940f6ef17a79ca98120b7ac188f126c3c9b6fe7
George Broughton <broughton_g182@hotmail.com>
2020-01-29 10:07:00 +0400

    spray server

--- src/spray.cpp
+++ src/spray.cpp
@@ -32,18 +32,22 @@ typedef enum{
 	FAIL
 }ESprayState;
 
-int misdetections = 0;
+//int misdetections = 0;
+ros::NodeHandle n;
 ESprayState state = IDLE;
 ros::Publisher cmd_vel;
 geometry_msgs::Twist base_cmd;
-ros::Subscriber thermalSubscriber;
+ros::Subscriber thermalSubscriber; //only subscribe when in use: n.subscribe("/thermal/raw_temp_array", 1, thermalCallback);
 
 CTimer timer;
 CPump *pump;
 
+int nThermalFrames = 0;
+int nThermalFramesReq = 5;
+
 /*Parameters we intend to make dynamically adjustable*/
 float fwSpeed = 0.1f;
-float rotationCoefficient = 30.0f; //bigger is slower
+float rotationCoefficient = 40.0f; //bigger is slower
 float fireThreshold = 45.0f;
 int sprayTime = 1000; //in miliseconds
 
@@ -61,18 +65,26 @@ int sprayTime = 1000; //in miliseconds
 	dispConst=config.dispConst; 
 	
 }*/
+void thermalCallback(const std_msgs::String::ConstPtr& msg);
+void startTurning()
+{
+  thermalSubscriber = n.subscribe("/thermal/raw_temp_array", 1, thermalCallback);
+  state = TURNING;
+  nThermalFrames = 0;
+}
+
+void stopTurning()
+{
+  thermalSubscriber.shutdown();
+  state = APPROACHING;
+}
 
 void thermalCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("Thermal message received");
-
-
-  if(!(state == TURNING ||
-      state == APPROACHING ||
-//      state == ELEVATING ||
-      state == SPRAYING))
+  if(state != TURNING)
   {
-    //rejecting image
+    //rejecting image as we don't need it right not
     return;
   }
   
@@ -80,7 +92,7 @@ void thermalCallback(const std_msgs::String::ConstPtr& msg)
   int cameraHeight = 32;
   int columnPeaks[cameraWidth];
 
-  /*for(int colIdx = 0; colIdx < cameraWidth; colIdx++)
+  for(int colIdx = 0; colIdx < cameraWidth; colIdx++)
   {
     for(int rowIdx = 0; rowIdx < cameraHeight; rowIdx++)
     {
@@ -90,7 +102,7 @@ void thermalCallback(const std_msgs::String::ConstPtr& msg)
         columnPeaks[colIdx] = (float)msg->data[messageIndex];      
       }
     }
-  }*/
+  }
 
   float peak = 0.0f;
   int peakIdx = -1;
@@ -111,16 +123,41 @@ void thermalCallback(const std_msgs::String::ConstPtr& msg)
   }
 
   int midPoint = cameraWidth / 2;
+  int centredThresh = 2;
+  if(peakIdx > (midPoint - centredThresh) && peakIdx < (midPoint + centredThresh)) 
+  {
+    if(nThermalFrames >= nThermalFramesReq)
+    {
+      stopTurning();
+      return;
+    }
+    else
+    {
+      nThermalFrames++;
+    }
+  }
+
   float rotationalVelocity = -((peakIdx-midPoint) / rotationCoefficient);
 
   base_cmd.angular.z = rotationalVelocity;
   cmd_vel.publish(base_cmd);
 }
 
+void startSpraying()
+{
+  timer.reset();
+  timer.start();
+  state = SPRAYING;
+  pump->on();
+}
+
 bool isTerminal(ESprayState state)
 {
- 	if (state == ALIGNING) return false;
- 	if (state == SPRAYING) return false;
+	if(state == TURNING) return false;
+	if(state == APPROACHING) return false;
+	if(state == ALIGNING) return false;
+	if(state == SPRAYING) return false;
+  if(state == FINAL) return true;
 	return true;
 }
 
@@ -128,25 +165,43 @@ void actionServerCallback(const mbzirc_husky::sprayGoalConstPtr &goal, Server* a
 {
 	sprayTime = goal->duration;
 	mbzirc_husky::sprayResult result;
-	if (state == IDLE){
+
+  startTurning();
+
+	/*if (state == IDLE){
 		state = SPRAYING;
 		timer.reset();
 		timer.start();
 		pump->on();
-	}
+	}*/
 	while (isTerminal(state) == false){
+    if(state == TURNING)
+      //handled inside of themal callback
+    if(state == APPROACHING)
+    {
+      //move towards target
+      state = ALIGNING;
+    }
+    if(state == ALIGNING)
+    {
+      //move arm to desired position
+      startSpraying();
+    }
 		if (state == SPRAYING && timer.getTime() > sprayTime){
-		       	state = FINAL;
+     	state = FINAL;
 			pump->off();
 		}
-		usleep(15000);
+		usleep(100);
 	}
 	pump->off();
 	if (state == FINAL) state = SUCCESS; else state = FAIL;
 	if (state == SUCCESS) 	server->setSucceeded(result);
 	if (state == FAIL) 	server->setAborted(result);
 	if (state == PREEMPTED) server->setPreempted(result);
-	state = STOPPING;
+	//state = STOPPING;
+	
+  base_cmd.linear.x = base_cmd.angular.z = 0;
+	cmd_vel.publish(base_cmd);
 	state = IDLE;
 }
 
@@ -155,7 +210,7 @@ int main(int argc, char** argv)
 {
 	pump = new CPump("/dev/robot/pump");
 	ros::init(argc, argv, "spray");
-	ros::NodeHandle n;
+	//ros::NodeHandle n;
 	// Dynamic reconfiguration server
 	/*dynamic_reconfigure::Server<mbzirc_husky::sprayConfig> dynServer;
   	dynamic_reconfigure::Server<mbzirc_husky::sprayConfig>::CallbackType f = boost::bind(&callback, _1, _2);
@@ -165,14 +220,14 @@ int main(int argc, char** argv)
 	cmd_vel = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 	server = new Server(n, "sprayServer", boost::bind(&actionServerCallback, _1, server), false);
 	server->start();
-  ros::Subscriber sub = n.subscribe("/thermal/raw_temp_array", 1, thermalCallback);
   //scan_sub = n.subscribe("scan", 100, scanCallback);
 	//robot_pose = n.subscribe("/robot_pose", 1000, poseCallback);
 	//:point_pub_ = n.advertise<pcl::PointCloud<pcl::PointXYZ> > ("/points2", 100000);
 	while (ros::ok()){
 		if (server->isPreemptRequested() && state != IDLE) state = PREEMPTED;
 		if (state == STOPPING){
-			base_cmd.linear.x = base_cmd.angular.z = 0;
+      pump->off();
+      base_cmd.linear.x = base_cmd.angular.z = 0;
 			cmd_vel.publish(base_cmd);
 			state = IDLE;
 		} 
