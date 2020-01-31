9e60e07b8c59495da12b1ef96b9e6a7f0923c0db
George Broughton <broughton_g182@hotmail.com>
2020-01-31 16:56:03 +0400

    fixed broken launch files from new folder, change sim backend

---
+++ sensor_control/simulation/fire/createScene.py
@@ -0,0 +1,28 @@
+import random
+import sys
+sys.path.append('.')
+sys.path.append('./data')
+sys.path.append('./src')
+
+#import husky
+from morse.builder import *
+from src.fire.builder.robots import Husky
+
+husky = Husky()
+#husky.translate(0, 0, 0)
+#husky.add_default_interface('ros')
+
+#add robot to scene
+#robot = husky.createHusky()
+#robot.translate(0, 0, 0)
+#robot.rotate(0, 0, random.uniform(0, 1) - 0.5)
+
+#add scene
+building = PassiveObject('../models/tower.blend','Object002')
+building.translate(x=random.uniform(15, 20), y=random.uniform(7, -9), z=0)
+
+env = Environment('empty', fastmode = True)
+env.set_camera_location([-18.0, -6.7, 10.8])
+env.set_camera_rotation([1.09, 0, -1.14])
+env.show_framerate(True)
+
---
+++ sensor_control/simulation/old/fire/tutorial-1-ros.py
@@ -0,0 +1,15 @@
+from morse.builder import *
+
+atrv = ATRV()
+
+pose = Pose()
+pose.translate(z = 0.75)
+atrv.append(pose)
+
+motion = MotionVW()
+atrv.append(motion)
+
+# Add default interface for our robot's components
+atrv.add_default_interface('ros')
+
+env = Environment('indoors-1/indoor-1')
