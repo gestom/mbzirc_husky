c36cf5a49af2debfca1f529c1836e2f3c9e8203c
majerfil <majerfil@fel.cvut.cz>
2020-02-01 21:40:51 +0400

    towards pile detection

--- sensor_control/src/brickExplore.cpp
+++ sensor_control/src/brickExplore.cpp
@@ -6,9 +6,19 @@
 #include <actionlib/server/simple_action_server.h>
 #include <dynamic_reconfigure/server.h>
 #include <dynamic_reconfigure/Config.h>
+#include <mbzirc_husky/brick_pileConfig.h>
+
+//For labelling the brick pile
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+
 
 typedef actionlib::SimpleActionServer<mbzirc_husky::brickExploreAction> Server;
 Server *server;
+
+ros::Subscriber scan_sub;
+ros::Publisher point_pub;
 
 typedef enum{
 	IDLE = 0,
@@ -22,12 +32,134 @@ typedef enum{
 ESprayState state = IDLE;
 ros::NodeHandle* pn;
 
+float tolerance = 0.025;
+float angleTolerance = 0.50;
+float distanceTolerance = 0.25;
+float distance = 2.80;
+int minPoints = 0;
+int maxEvalu = 360;
+float spdLimit = 0.3;
+float realConst = 1;
+float dispConst = 3; 
+float fwSpeed = 0.1;
+
+int misdetections = 0;
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+PointCloud::Ptr pcl_msg (new PointCloud);
+
+void callback(mbzirc_husky::brick_pileConfig &config, uint32_t level) {
+
+        tolerance=config.tolerance;
+        angleTolerance=config.angleTolerance;
+        distanceTolerance=config.distanceTolerance;
+        distance=config.distance;
+        fwSpeed=config.fwSpeed;
+        minPoints=config.minPoints;
+        maxEvalu=config.maxEval;
+        spdLimit=config.spdLimit;
+        realConst=config.realConst;
+        dispConst=config.dispConst;
+
+}
+
+
+
+
 bool isTerminal(ESprayState state)
 {
 	if(state == EXPLORING) return false;
-    if(state == FINAL) return true;
+	if(state == FINAL) return true;
 	return true;
 }
+
+void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_msg)
+{
+	//	if(state==EXPLORING){
+
+	size_t num_ranges = scan_msg->ranges.size();
+	float x[num_ranges];
+	float y[num_ranges];
+	bool m[num_ranges];
+	float angle;
+	misdetections++;
+	for (int i = 0; i <= num_ranges; i++){
+		angle = scan_msg->angle_min+i*scan_msg->angle_increment;
+		x[i] = scan_msg->ranges[i]*cos(angle);
+		y[i] = scan_msg->ranges[i]*sin(angle);
+		m[i] = true;
+	}
+	int eval = 0;
+	int max_iterations = 1000;
+	int numHypotheses = 10;
+	float a,b;
+	float maxA[numHypotheses];
+	float maxB[numHypotheses];
+	int maxEval[numHypotheses];
+	for (int h = 0; h <= numHypotheses; h++){
+		maxEval[h] = 0;
+		for (int i = 0; i <= max_iterations; i++)
+		{
+			int aIndex = rand()%num_ranges; 
+			int bIndex = rand()%num_ranges;
+			while (bIndex == aIndex) bIndex = rand()%num_ranges;
+			a = (y[bIndex]-y[aIndex])/(x[bIndex]-x[aIndex]);	
+			b = y[bIndex]-a*x[bIndex];
+			eval = 0;
+			for (int j = 0; j <= num_ranges; j++){
+				if (fabs(a*x[j]-y[j]+b)<tolerance && m[j]) eval++;
+			}
+			if (maxEval[h] < eval){
+				maxEval[h]=eval;
+				maxA[h]=a;
+				maxB[h]=b;
+			}
+		}
+		for (int j = 0; j <= num_ranges; j++){
+			if (fabs(maxA[h]*x[j]-y[j]+maxB[h])<tolerance && m[j]) m[j] = false;
+		}
+	}
+
+	int b1 = -1;
+	int b2 = -1;
+	eval = 0; 
+	float realDist,realAngle,displacement;
+	/*for (int h1 = 0; h1 <= numHypotheses; h1++){
+		for (int h2 = h1+1; h2 <= numHypotheses; h2++){
+			if (fabs(maxA[h1]-maxA[h2]) < angleTolerance && maxEval[h1] > minPoints && maxEval[h2] > minPoints ){
+				realAngle = (maxA[h1]+maxA[h2])/2.0;
+				realDist = fabs(maxB[h1]-maxB[h2])*cos(atan(realAngle));
+				fprintf(stdout,"Ramp hypothesis: %i %i %f %f %i %i\n",h1,h2,realDist,fabs(maxA[h1]-maxA[h2]),maxEval[h1],maxEval[h2]);
+				if (fabs(realDist-distance)<distanceTolerance){
+					if (maxEval[h1]+maxEval[h2] > eval){
+						eval = maxEval[h1] + maxEval[h2];
+						b1 = h1;
+						b2 = h2;
+					}
+				}
+			}
+		}
+	}*/
+	//if (b1 >= 0 && b2 >=0)
+	//{
+
+		pcl_msg->header.frame_id = "laser";
+		pcl_msg->height = 1;
+		pcl_msg->points.clear();
+		pcl_msg->width = 0;
+		for (int j = 0; j <= num_ranges; j++)
+		{
+			//if (fabs(maxA[b1]*x[j]-y[j]+maxB[b1])<tolerance || fabs(maxA[b2]*x[j]-y[j]+maxB[b2])<tolerance){
+				pcl_msg->points.push_back (pcl::PointXYZ(x[j], y[j], 0.4));
+				pcl_msg->width++;
+
+		//		}
+		}
+		pcl_conversions::toPCL(ros::Time::now(), pcl_msg->header.stamp);
+		point_pub.publish (pcl_msg);
+	//}
+	//	}		
+}	
 
 void actionServerCallback(const mbzirc_husky::brickExploreGoalConstPtr &goal, Server* as)
 {
@@ -55,12 +187,13 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "brickExplore");
 	ros::NodeHandle n;
-    pn = &n;
+   	pn = &n;
 	// Dynamic reconfiguration server
-	/*dynamic_reconfigure::Server<mbzirc_husky::sprayConfig> dynServer;
-  	dynamic_reconfigure::Server<mbzirc_husky::sprayConfig>::CallbackType f = boost::bind(&callback, _1, _2);
-  	dynServer.setCallback(f);*/
-
+	dynamic_reconfigure::Server<mbzirc_husky::brick_pileConfig> dynServer;
+  	dynamic_reconfigure::Server<mbzirc_husky::brick_pileConfig>::CallbackType f = boost::bind(&callback, _1, _2);
+  	dynServer.setCallback(f);
+	scan_sub = n.subscribe("/scan",100, scanCallback);	
+	point_pub = n.advertise<sensor_msgs::PointCloud2>("boxes/correct",10);
 	server = new Server(n, "brickExploreServer", boost::bind(&actionServerCallback, _1, server), false);
 	server->start();
 	while (ros::ok()){
