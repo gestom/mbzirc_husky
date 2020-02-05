2f52323f54aeeafb0cd190b92bfc256c06d1e228
Husky Robot <husky@robot>
2020-02-05 09:04:24 +0100

    battery warning in robot launch and correct message type

--- sensor_control/scripts/battery.py
+++ sensor_control/scripts/battery.py
@@ -1,26 +1,28 @@
-# import yagmail module.
+#!/usr/bin/env python
 import yagmail
 import rospy
 import time
+from husky_msgs.msg import HuskyStatus
 
 on = True
 
 def callback(msg):
     global on
-    if msg.voltage < 24 and on:
+    if float(msg.battery_voltage) < 24 and on:
         on = False
         sendEmail()
 
 def sendEmail():
+    print("sending battery warning email")
     yag_smtp_connection = yagmail.SMTP( user="husky.chrono@gmail.com", password="huskyhusky", host='smtp.gmail.com')
     subject = 'Change my battery'
     contents = ['Hello this is husky speaking', 'Change my battery']
     yag_smtp_connection.send('broughtong92@gmail.com', subject, contents)
     time.sleep(1000)
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     rospy.init_node("battery_email")
-    rospy.Subscriber("/status", , callback)
+    rospy.Subscriber("/status", HuskyStatus, callback)
     rospy.spin()
 
 
