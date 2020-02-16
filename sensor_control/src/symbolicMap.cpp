a987fb87568de479ae0ff9a5a7bc89ddb0d0d2a9
Husky Robot <husky@robot>
2020-02-16 20:51:55 +0100

    tsp prelim

--- sensor_control/src/symbolicMap.cpp
+++ sensor_control/src/symbolicMap.cpp
@@ -12,10 +12,13 @@
 #include <opencv2/core/types.hpp>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include "order.h"
 
 ros::NodeHandle* pn;
 
 ros::Publisher waypointVisualiser;
+ros::Publisher waypointRangeVisualiser;
 
 //Map reconfigure
 double map_width;
@@ -25,6 +28,7 @@ double map_y;
 double map_rotation;
 
 int waypointIdx = 0;
+float maxObservationDistance = 4.0f;
 
 //Clustering reconfigure
 double tolerance;
@@ -39,6 +43,7 @@ std::vector<cv::Point2d> greenBricks;
 std::vector<cv::Point2d> orangeBricks;
 
 void publishWaypoints();
+void organisePath();
 
 void callback(mbzirc_husky::boundsConfig &config, uint32_t level) {
         map_width=config.w;
@@ -254,6 +259,7 @@ bool getPointCallback(mbzirc_husky::getPoi::Request &req, mbzirc_husky::getPoi::
 	ROS_INFO("Incomin service of type %i ", req.type);	
 	switch (incoming_type){
 		case 0: if(!waypoints.empty()){
+                		publishWaypoints();
 				point = waypoints[waypointIdx];
 				res.x = point.x;
 				res.y = point.y;
@@ -261,7 +267,6 @@ bool getPointCallback(mbzirc_husky::getPoi::Request &req, mbzirc_husky::getPoi::
 				waypointIdx++;
 				if(waypointIdx > (waypoints.size()-1)) waypointIdx = 0; 
 				ROS_INFO("Retrieving the next point from the waypoints symbolic map, point type %i at position X: %f Y: %f", req.type,point.x,point.y);
-                publishWaypoints();
 				return true;
 			}
 			else {
@@ -386,18 +391,59 @@ void loadWaypoints(){
 	}
 	ROS_INFO("Loaded waypoints with %i points",(int)waypoints.size());
 	loadFile.close();
-    publishWaypoints();
+    	organisePath();
+	publishWaypoints();
+}
+
+void organisePath()
+{
+	float* xs = new float[waypoints.size()];
+	float* ys = new float[waypoints.size()];
+
+	for(int i = 0; i < waypoints.size(); i++)
+	{
+		xs[i] = waypoints[i].x;
+		ys[i] = waypoints[i].y;
+		printf("%f %f\n", xs[i], ys[i]);
+	}
+
+	CTSP pf(xs, ys, waypoints.size());
+	pf.solve(3);
+	pf.print();
+	delete[] xs;
+	delete[] ys;
 }
 
 void publishWaypoints()
 {
-    for(int i = 0; i < waypoints.size() - 1; i++)
+    ROS_INFO("Publishing waypoints");
+    for(int i = 0; i < waypoints.size(); i++)
     {
         visualization_msgs::Marker msg;
         msg.header.frame_id = "map";
         msg.header.stamp = ros::Time::now();
-        msg.id = 0;
+        msg.id = i;
         msg.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+
+        msg.pose.position.x = waypoints[i].x;
+        msg.pose.position.y = waypoints[i].y;
+        msg.pose.position.z = 0.2;
+
+        msg.color.a = 1;
+        msg.color.r = 1;
+        msg.color.g = 0;
+        msg.color.b = 1;
+
+        msg.scale.z = 1;
+       
+       	std::ostringstream stream;
+        stream << (i - waypointIdx) % waypoints.size() ;
+     	msg.text = stream.str();	
+	waypointVisualiser.publish(msg);
+
+        msg.header.frame_id = "map";
+        msg.header.stamp = ros::Time::now();
+        msg.type = visualization_msgs::Marker::CYLINDER;
 
         msg.pose.position.x = waypoints[i].x;
         msg.pose.position.y = waypoints[i].y;
@@ -405,11 +451,15 @@ void publishWaypoints()
 
         msg.color.a = 1;
         msg.color.r = 1;
-        msg.color.g = 0;
-        msg.color.b = 1;
+        msg.color.g = 1;
+        msg.color.b = 0;
 
-        msg.scale.z = 25;
-        msg.text = "1";
+	msg.scale.x = maxObservationDistance*2;
+	msg.scale.y = maxObservationDistance*2;
+        msg.scale.z = 0.01;
+       
+	waypointRangeVisualiser.publish(msg);
+
     }
 }
 
@@ -420,7 +470,8 @@ int main(int argc, char** argv)
 	ros::NodeHandle n;
 	pn = &n;
 
-    waypointVisualiser = n.advertise<visualization_msgs::Marker>("/symbolicMap/waypoints", 1);
+    waypointVisualiser = n.advertise<visualization_msgs::Marker>("/symbolicMap/waypoints", 100);
+    waypointRangeVisualiser = n.advertise<visualization_msgs::Marker>("/symbolicMap/waypointRanges", 100);
 
 	// service servers
 	ros::ServiceServer set_map_srv = n.advertiseService("set_map_poi", setPointCallback);
